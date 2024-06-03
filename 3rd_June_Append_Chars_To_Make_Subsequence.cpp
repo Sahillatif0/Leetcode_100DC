60f929937a548bdb9a72e8d2602ae428dbfd399a
Sahil Latif <sahillatif072@gmail.com>
2024-06-03 05:31:15 +0500

    added 3rd_June_Append_Chars_To_Make_Subsequence

---
+++ 3rd_June_Append_Chars_To_Make_Subsequence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i=0,j=0;
+        for(int i=0;i<s.size();i++)
+            if(s[i]==t[j])
+                t[j++] = '0';
+        int cnt=0;
+        for(int i=0;i<t.size();i++)
+            if(t[i]!='0')
+                cnt++;
+        return t.size();
+    }
+};
+int main() {
+    Solution s;
+    cout<<s.appendCharacters("coaching", "a");
+}
